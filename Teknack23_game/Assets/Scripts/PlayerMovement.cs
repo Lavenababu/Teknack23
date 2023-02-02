6184430e43de589811c2927992740df133357394
mohd88798 <mohdzaid88798@gmail.com>
2023-02-02 11:45:43 +0530

    added player movement

--- Teknack23_game/Assets/Scripts/PlayerMovement.cs
+++ Teknack23_game/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float jumpForce = 10f;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rigidBody;
+
+    private void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        rigidBody.velocity = new Vector2(moveSpeed, rigidBody.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+    //     Vector2 position = transform.position;
+    //     Vector2 direction = Vector2.down;
+    //     float distance = 0.5f;
+
+    //     RaycastHit2D hit = Physics2D.BoxCast(position, new Vector2(0.5f, 0.5f), 0f, direction, distance, LayerMask.GetMask("Ground"));
+
+    //     return hit.collider != null;
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.5f);
     }
 }
